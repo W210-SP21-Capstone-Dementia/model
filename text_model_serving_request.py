a1b18076d95c1e0286aee473657adc8dc3c4c442
James Wall <james.wall@wustl.edu>
2021-03-03 11:53:07 -0800

    Pretty sure flask app is working locally, need to test prediction still

--- text_model_serving_request.py
+++ text_model_serving_request.py
@@ -1,21 +1,54 @@
 import joblib
 import pandas as pd
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+# These will be populated at training time
+model_columns = None
+clf = None
 
 # Predict method for API call
 @app.route('/predict', methods=['POST'])
 def predict():
-     json_ = request.json
-     query_df = pd.DataFrame(json_)
-     query = pd.get_dummies(query_df)
-     prediction = clf.predict(query)
-     return jsonify({'prediction': list(prediction)})
+    if clf:
+        try:
+            json_ = request.json
+            query_df = pd.DataFrame(json_)
+            query = pd.get_dummies(query_df)
+
+            # We have the list of columns persisted, 
+            # so we can just replace the missing values with zeros 
+            # at the time of prediction.
+            query = query.reindex(columns=model_columns, fill_value=0)
+
+            prediction = list(clf.predict(query))
+
+            # Converting to int from int64
+            return jsonify({"prediction": list(map(int, prediction))})
+        except Exception as e:
+
+            return jsonify({'error': str(e), 'trace': traceback.format_exc()})
+    else:
+        print('train first')
+        return 'no model here'
 
 #  (1) load our persisted model into memory when the application starts
 #  (2) create an endpoint that takes input variables, 
 #  transforms them into the appropriate format, and returns predictions.
 if __name__ == '__main__':
-     clf = joblib.load('logistic_regression_model.pkl')
-     app.run(port=8080)
+    try:
+        # Load persisted model
+        clf = joblib.load('logistic_regression_model.pkl')
+        print('model loaded')
+
+        # Also we have to load model columns when the application starts.
+        model_columns = joblib.load('logistic_regression_model_columns.pkl')
+        print('model columns loaded')
+    except Exception as e:
+        print('Either model or columns are missing')
+        print(str(e))
+        clf = None
+
+    # TODO - change port and host number
+    app.run(host='localhost', port=8080, debug=True)
